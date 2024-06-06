62d7600fe5bfd23c9802fd6568ad39ea059242b7
Alisha Yu <alisha.yu15@gmail.com>
2024-06-06 17:58:02 -0400

    created vehicles table

--- setup.sql
+++ setup.sql
@@ -16,3 +16,15 @@ CREATE TABLE dealerships(
     phone varchar(12),
     PRIMARY KEY(dealership_id)
 );
+
+CREATE TABLE vehicles(
+	vin int NOT NULL,
+    year int NOT NULL,
+    make varchar(50) NOT NULL,
+    model varchar(50) NOT NULL,
+    vehicle_type varchar(30),
+    color varchar(30) NOT NULL,
+    odometer int NOT NULL,
+    price double NOT NULL,
+    PRIMARY KEY(vin)
+);
